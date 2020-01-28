4ead4def910a61675e91202d390082afdcd3d394
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2020-01-27 22:07:46 -0600

    finished final functionality

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -6,7 +6,7 @@ public class ConsoleExercises {
         System.out.printf("The value of pi is approximately %.2f. \n", pi); //or System.out.format
 
 
-//        ======user input prompt======
+//        //======user input prompt======
 //        Scanner scanner = new Scanner(System.in);
 //
 //        System.out.println("Enter something: ");
@@ -15,7 +15,7 @@ public class ConsoleExercises {
 //        System.out.println("You entered: --> \"" + userInput + "\" <--");
 
 
-//        =======int user input prompt======
+        //=======int user input prompt======
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.print("Enter a number: ");
 //        // nextInt() reads the next integer from the keyboard
@@ -35,7 +35,7 @@ public class ConsoleExercises {
 //        System.out.printf("You entered: %s %s %s \n",userInput1, userInput2, userInput3); //or System.out.format
 
 
-//        =======user sentence string =====
+        //=====user sentence string =====
 
 //        Scanner scanner = new Scanner(System.in);
 ////
@@ -46,17 +46,31 @@ public class ConsoleExercises {
 //======= classroom size ======
 
         Scanner scanner = new Scanner(System.in);
-//
+
 //        System.out.println("Enter the width and height: ");
 //        Integer width = scanner.nextInt();
 //        Integer height = scanner.nextInt();
-
+//
 //        System.out.println("Area is: --> \"" + width * height + "\" <--");
 
-//        System.out.println("Enter the width and height: ");
-//        String input = scanner.next();;
-//        int result = Integer.parseInt(input);
-//        System.out.println(result);
+
+        System.out.println("Enter the width and height: ");
+        String input = scanner.nextLine(); //Example input: 2.5 10.0
+
+        //Get location of space in order to separate values.  This only works if the user enters 2 values separated by a space.
+        int space = input.indexOf(' ');
+
+        //Parse the values, using the index of the space
+        float w = Float.parseFloat(input.substring(0, space));
+        float l = Float.parseFloat(input.substring(space, input.length()));
+
+        //Calculate area/circumference
+        float area = w*l;
+        float perimeter = 2*w + 2*l;
+
+//        System.out.println("Area is: --> \"" + area + "\" <-- and Perimeter is: --> \"" + perimeter + "\"");
+        System.out.printf("Area is: --> %f <-- and Perimeter is: --> %.2f",area, perimeter); //or System.out.format
+
 
     }
 
