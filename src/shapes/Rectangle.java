9f53aae86ead8fa5788bd0319f37993995493a86
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2020-02-05 08:49:41 -0600

    shapes walkthrough

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -3,18 +3,17 @@ package shapes;
 public class Rectangle {
     protected int length;
     protected int width;
-//    Rectangle should define a constructor that accepts two numbers for length and width, and sets those properties.
+    protected int side;
+    //    Rectangle should define a constructor that accepts two numbers for length and width, and sets those properties.
 public Rectangle(int widthNum, int lengthNum) {
     this.width = widthNum;
     this.length = lengthNum;
 }
-//public Square(){}
-
 public int getArea() {
     return width * length;
 }
 public int getPerimeter() {
-    return 2 * length + 2 * width;
+    return (2 * length) + (2 * width);
 }
 
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,14 +1,17 @@
 package shapes;
 
 public class Square extends Rectangle {
-    protected int side;
 
     public Square(int side) {
         super(side, side);
+        this.side = side;
     }
+
+    @Override
     public int getArea() {
-        return side ^ 2;
+        return side * side;
     }
+    @Override
     public int getPerimeter() {
         return 4 * side;
     }
